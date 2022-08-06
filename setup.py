d66ff162b9a64f962ed2dd9fba4ecc5f1583f311
Andrea Sala <andrea.sala98@gmail.com>
2022-08-06 20:01:41 +0200

    stp

--- setup.py
+++ setup.py
@@ -10,13 +10,17 @@ here = os.path.abspath(os.path.dirname(__file__))
 def get_version():
     """ Gets the version from the package's __init__ file
     if there is some problem, let it happily fail """
-    VERSIONFILE = os.path.join(here, "src", PACKAGE, "__init__.py")
-    initfile_lines = open(VERSIONFILE, "rt").readlines()
-    VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
-    for line in initfile_lines:
-        mo = re.search(VSRE, line, re.M)
-        if mo:
-            return mo.group(1)
+
+    try:
+        VERSIONFILE = os.path.join(here, "src", PACKAGE, "__init__.py")
+        initfile_lines = open(VERSIONFILE, "rt").readlines()
+        VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
+        for line in initfile_lines:
+            mo = re.search(VSRE, line, re.M)
+            if mo:
+                return mo.group(1)
+    except:
+        return 'n/d'
 
 # load long description from README
 
